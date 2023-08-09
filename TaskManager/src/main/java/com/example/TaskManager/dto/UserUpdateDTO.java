61e04af05d00b32f8c296cd70803e5b02b993387
Euder de Sousa <euderdesousaaaa@gmail.com>
2023-08-09 12:24:56 -0300

    UserUpdate receive the responsibility of change just Name,Login and email

--- TaskManager/src/main/java/com/example/TaskManager/dto/UserUpdateDTO.java
+++ TaskManager/src/main/java/com/example/TaskManager/dto/UserUpdateDTO.java
@@ -1,11 +1,25 @@
 package com.example.TaskManager.dto;
 
+import com.example.TaskManager.entities.enums.Roles;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserUpdateDTO extends UserDTO {
 
+    private String name;
+
+    private String login;
+
+    @Email
+    private String email;
 }
