088cbcc3fb40139718ce15435da987c04f6cf0de
Euder de Sousa <euderdesousaaaa@gmail.com>
2023-08-09 12:36:38 -0300

    Create login

--- TaskManager/src/main/java/com/example/TaskManager/repository/UserRepository.java
+++ TaskManager/src/main/java/com/example/TaskManager/repository/UserRepository.java
@@ -2,9 +2,13 @@ package com.example.TaskManager.repository;
 
 import com.example.TaskManager.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmail(String email);
+
+    UserDetails findByLogin(String login);
+
 }
