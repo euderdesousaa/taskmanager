560bd7f3feda0a39e480dd02c62af9b5f4170482
Euder de Sousa <euderdesousaaaa@gmail.com>
2023-08-09 12:33:23 -0300

    Refactoring the class to handle projects and tasks at the same time

--- TaskManager/src/main/java/com/example/TaskManager/service/ProjectService.java
+++ TaskManager/src/main/java/com/example/TaskManager/service/ProjectService.java
@@ -1,73 +1,57 @@
 package com.example.TaskManager.service;
 
 import com.example.TaskManager.dto.ProjectDTO;
+import com.example.TaskManager.dto.ProjectInsertDTO;
 import com.example.TaskManager.entities.Project;
+import com.example.TaskManager.entities.Task;
+import com.example.TaskManager.mapper.ProjectMapper;
 import com.example.TaskManager.repository.ProjectRepository;
+import com.example.TaskManager.repository.TaskRepository;
 import com.example.TaskManager.service.exceptions.DatabaseException;
-import com.example.TaskManager.service.exceptions.EntityNotFoundException;
 import com.example.TaskManager.service.exceptions.ResourceNotFoundException;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Service
+@RequiredArgsConstructor
 public class ProjectService {
 
-    @Autowired
-    private ProjectRepository repository;
+    private final ProjectRepository projectRepository;
+    private final TaskRepository taskRepository;
+    private final ProjectMapper projectMapper;
 
-    public List<ProjectDTO> findAllPaged() {
-        List<Project> list = repository.findAll();
-        return list.stream().map(x -> new ProjectDTO(x)).collect(Collectors.toList());
+    public ProjectDTO insertProject(ProjectInsertDTO dto) {
+        Project save = projectRepository.save(projectMapper.toEntityInsert(dto));
+        return projectMapper.toDTO(save);
     }
 
-    public ProjectDTO findById(Long id) {
-        Optional<Project> obj = repository.findById(id);
-        Project entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
-
+    @Transactional(readOnly = true)
+    public ProjectDTO getProjectById(Long projectId) {
+        Optional<Project> obj = projectRepository.findById(projectId);
+        Project entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity Not Found"));
         return new ProjectDTO(entity);
     }
 
-    public ProjectDTO update(Long id, ProjectDTO dto) {
-        try {
-            Project entity = repository.getReferenceById(id);
-            copyDTOEntity(dto, entity);
-            entity = repository.save(entity);
-            return new ProjectDTO(entity);
-        } catch (EntityNotFoundException e) {
-            throw new ResourceNotFoundException("id Not found" + id);
-        }
+    public void addTaskToProject(Long projectId, Task task) {
+        Project project = projectRepository.findById(projectId).orElseThrow(() -> new IllegalArgumentException("Project not Found"));
+        project.setTasks(project.getTasks());
+        task.setProject(project);
+        taskRepository.save(task);
     }
 
-    public ProjectDTO insert(ProjectDTO dto) {
-        Project entity = new Project();
-        copyDTOEntity(dto, entity);
-        entity = repository.save(entity);
-        return new ProjectDTO(entity);
-    }
-
-    public void delete(Long id) {
+    public void deleteTaskFromProject(Long taskId) {
         try {
-            repository.deleteById(id);
-        } catch (
-                EmptyResultDataAccessException e) {
-            throw new ResourceNotFoundException("id not found" + id);
-        } catch (
-                DataIntegrityViolationException e) {
+            taskRepository.deleteById(taskId);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException("id not found" + taskId);
+        } catch (DataIntegrityViolationException e) {
             throw new DatabaseException("Integrity violation");
         }
     }
 
-    private void copyDTOEntity(ProjectDTO dto, Project entity) {
-        entity.setName(dto.getName());
-        entity.setDescription(dto.getDescription());
-        entity.setStatus(dto.getStatus());
-        entity.setInit_date(dto.getInit_date());
-        entity.setEnd_date(dto.getEnd_date());
-    }
 }
